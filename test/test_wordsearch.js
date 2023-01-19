4f962be95478c69dd852d62918470aee274ee59e
Xuan Duong Ngo <ngoxd97@gmail.com>
2023-01-18 21:32:53 -0700

    solution

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -31,7 +31,55 @@ describe("#wordSearch()", function() {
       ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-    ], 'SEINFELD')
+    ], 'SEINFELd')
+
+    assert.isTrue(result);
+  });
+
+  it("should return true if the word is present vertically", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'fnft')
+
+    assert.isTrue(result);
+  });
+
+  it("should return true if the word is present backward", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'kze')
+
+    assert.isTrue(result);
+  });
+
+  it("should return true if the word is present vertically backwards", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'lsibl')
 
     assert.isTrue(result);
   });
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,37 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+// const wordSearch = (letters, word) => { 
+//     const horizontalJoin = letters.map(ls => ls.join(''))
+//     for (l of horizontalJoin) {
+//         if (l.includes(word)) return true
+//     }
+// }
+
+const wordSearch = function(letters, word) {
+  // const newArray = transpose(letter);
+  let wordBackwards = word.split('');
+  wordBackwards = wordBackwards.reverse();
+  wordBackwards = wordBackwards.join('').toLowerCase();
+
+  if (word === undefined) {
+    return false;
+  }
+
+  for (let i = 0; i < letters.length; i++) {
+    if (letters[i].join("").toLowerCase().includes(word.toLowerCase()) || letters[i].join("").toLowerCase().includes(wordBackwards)) {
+      return true;
     }
+  }
+
+  for (let i = 0; i < letters[0].length; i++) {
+    let verticalWord = '';
+    for (let j = 0; j < letters.length; j++) {
+      verticalWord += letters[j][i].toLowerCase();
+    }
+    if (verticalWord.includes(word.toLowerCase()) || verticalWord.includes(wordBackwards)) {
+      return true;
+    }
+  }
+
+  return false;
 }
 
 module.exports = wordSearch
